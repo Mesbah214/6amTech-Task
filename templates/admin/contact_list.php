a03217a58bfdb54a03296b5f426440cce31fbad0
Mesbah Uddin <msbh214@gmail.com>
2025-04-17 03:31:05 +0600

    feat: added pot file & updated shortcode message

--- templates/admin/contact_list.php
+++ templates/admin/contact_list.php
@@ -1,6 +1,9 @@
 <div class="wrap">
     <h1><?php esc_html_e( 'Contact List', '6amtech_task' ); ?></h1>
-    <p><?php _e( 'The shortcode for the contact list table is [contact_list]', '6amtech_task' ); ?></p>
+    <p>
+        <?php _e( 'To show the contact list on the frontend, use the following shortcode:', '6amtech_task' ); ?>
+        <code>[contact_list]</code>
+    </p>
     <?php if ( isset( $_GET[ 'inserted' ] ) ) : ?>
         <div class="notice notice-success is-dismissible">
             <p><?php _e( 'Contact added successfully.', '6amtech_task' ); ?></p>
